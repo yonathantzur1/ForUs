7e6bac153118477afdbbd4800e8ab60100010790
yonathantzur1 <yonathantzur1@gmail.com>
2016-11-20 20:05:43 +0200

    Change DAL to connection pool work

--- modules/DAL.js
+++ modules/DAL.js
@@ -1,14 +1,40 @@
 var config = require('./config.js');
-
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
 // Connection URL
 var url = config.connectionString;
+var db;
+var poolSize = 10;
+var maxConnectRetry = 5;
+var retryCount = 0;
+
+GetDB = function (callback) {
+    if (!db || !db.serverConfig || !db.serverConfig.isConnected()) {
+        MongoClient.connect(url, { server: { maxPoolSize: poolSize } }, function (err, database) {
+            if (err == null) {
+                db = database;
+                callback(null, db);
+            }
+            else {
+                if (retryCount < maxConnectRetry) {
+                    retryCount++;
+                    getDB(callback);
+                }
+                else {
+                    callback(err, db);
+                }
+            }
+        });
+    }
+    else {
+        callback(null, db);
+    }
+}
 
 module.exports = {
     GetDocsByFilter: function (collectionName, filter, callback) {
-        MongoClient.connect(url, function (err, db) {
+        GetDB(function (err, db) {
             if (err == null) {
                 var collection = db.collection(collectionName);
                 collection.find(filter).toArray(function (err, docs) {
@@ -19,8 +45,6 @@ module.exports = {
                         callback(null);
                     }
                 });
-
-                db.close();
             }
             else {
                 callback(null);
@@ -29,7 +53,7 @@ module.exports = {
     },
 
     InsertDocument: function (collectionName, doc, callback) {
-        MongoClient.connect(url, function (err, db) {
+        GetDB(function (err, db) {
             if (err == null) {
                 var collection = db.collection(collectionName);
                 collection.insertOne(doc, function (err, result) {
@@ -40,8 +64,6 @@ module.exports = {
                         callback(null);
                     }
                 });
-
-                db.close();
             }
             else {
                 callback(null);
@@ -50,7 +72,7 @@ module.exports = {
     },
 
     UpdateDocument: function (collectionName, idObj, fieldToUpdateObj, callback) {
-        MongoClient.connect(url, function (err, db) {
+        GetDB(function (err, db) {
             if (err == null) {
                 var collection = db.collection(collectionName);
                 collection.findOneAndUpdate(idObj, { $set: fieldToUpdateObj }, {
@@ -69,8 +91,6 @@ module.exports = {
                             callback(null);
                         }
                     });
-
-                db.close();
             }
             else {
                 callback(null);
