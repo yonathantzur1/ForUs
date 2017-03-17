ac43e935269ae20f9f13c0e5c84af14d0589e1b9
yonathantzur1 <yonathantzur1@gmail.com>
2017-03-18 00:51:13 +0200

    Change db address

--- modules/config.js
+++ modules/config.js
@@ -1,4 +1,4 @@
-var url = 'mongodb://forusdb:Aa123456@ds021166.mlab.com:21166/forusdb';
+var url = 'mongodb://forusdb:Aa123456@ds145329.mlab.com:45329/forus';
 var poolSize = 10;
 
 module.exports = {
