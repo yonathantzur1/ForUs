8569dc65cdbe2a649bca42cb9ecffe7dc20b54d0
yonathantzur1 <yonathantzur1@gmail.com>
2019-10-13 16:49:21 +0300

    Insert server routes imports to main route file

---
+++ modules/routes/main.js
@@ -0,0 +1,68 @@
+const tokenHandler = require('../handlers/tokenHandler');
+const permissionsMiddleware = require('../middlewares/permissionsMiddleware');
+
+module.exports = (app, connectedUsers) => {
+    // Validate user token for each api request.
+    app.use('/api', Exclude([
+        '/login/*',
+        '/register/*',
+        '/forgotPassword/*',
+        '/deleteUser/*',
+        '/auth/isUserOnSession',
+        '/auth/isUserSocketConnect'
+    ], (req, res, next) => {
+        tokenHandler.validateUserAuthCookies(req) ? next() : res.sendStatus(401);
+    }));
+
+    app.use('/api/login', require('./welcome/login'));
+    app.use('/api/register', require('./welcome/register'));
+    app.use('/api/forgotPassword', require('./welcome/forgotPassword'));
+    app.use('/api/deleteUser', require('./deleteUser'));
+    app.use('/api/navbar', require('./navbar'));
+    app.use('/api/home', require('./home'));
+    app.use('/api/chat', require('./chat'));
+    app.use('/api/profilePicture', require('./profilePicture'));
+    app.use('/api/userPage', require('./userPage/userPage'));
+    app.use('/api/userEditWindow', require('./userPage/userEditWindow'));
+    app.use('/api/userReportWindow', require('./userPage/userReportWindow'));
+    app.use('/api/userPasswordWindow', require('./userPage/userPasswordWindow'));
+    app.use('/api/userPrivacyWindow', require('./userPage/userPrivacyWindow'));
+    app.use('/api/searchPage', require('./searchPage'));
+    app.use('/api/management', permissionsMiddleware.root, require('./managementPanel/management'));
+    app.use('/api/statistics', permissionsMiddleware.root, require('./managementPanel/statistics'));
+    app.use('/api/usersReports', permissionsMiddleware.root, require('./managementPanel/usersReports'));
+    app.use('/api/permissions', permissionsMiddleware.master, require('./managementPanel/permissions'));
+
+    app.use('/api/auth', (req, res, next) => {
+        req.connectedUsers = connectedUsers;
+        next();
+    }, require('./auth'));
+}
+
+// Exclude routes for middlewares.
+function Exclude(paths, middleware) {
+    return (req, res, next) => {
+        for (let i = 0; i < paths.length; i++) {
+            let path = paths[i];
+            let reqUrl = req.path;
+            let generalPathPosition = path.indexOf('/*');
+            let isExcludeMath;
+
+            //  In case the path is general and ends with /*
+            if (generalPathPosition != -1) {
+                path = path.substring(0, generalPathPosition);
+                isExcludeMath = (reqUrl.indexOf(path) == 0);
+            }
+            else {
+                isExcludeMath = (path == reqUrl);
+            }
+
+            // In case the exclude path is match to req path.
+            if (isExcludeMath) {
+                return next();
+            }
+        }
+
+        return middleware(req, res, next);
+    };
+}
--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const secure = require('ssl-express-www');
 const path = require('path');
 const compression = require('compression');
 const io = require('socket.io')(http);
-const tokenHandler = require('./modules/handlers/tokenHandler');
-const permissionsMiddleware = require('./modules/middlewares/permissionsMiddleware');
 const config = require('./config');
 const logger = require('./logger');
 const requestDataSizeLimit = '8mb';
@@ -29,78 +27,13 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static('./'));
-app.use(express.static('public'));
 app.use(compression());
-
-//#region Middlewares
-// Exclude routes for middlewares.
-function Exclude(paths, middleware) {
-    return (req, res, next) => {
-        for (let i = 0; i < paths.length; i++) {
-            let path = paths[i];
-            let reqUrl = req.path;
-            let generalPathPosition = path.indexOf("/*");
-            let isExcludeMath;
-
-            //  In case the path is general and ends with /*
-            if (generalPathPosition != -1) {
-                path = path.substring(0, generalPathPosition);
-                isExcludeMath = (reqUrl.indexOf(path) == 0);
-            }
-            else {
-                isExcludeMath = (path == reqUrl);
-            }
-
-            // In case the exclude path is match to req path.
-            if (isExcludeMath) {
-                return next();
-            }
-        }
-
-        return middleware(req, res, next);
-    };
-}
-
-// Validate user token for each api request.
-app.use('/api', Exclude([
-    '/login/*',
-    '/register/*',
-    '/forgotPassword/*',
-    '/deleteUser/*',
-    '/auth/isUserOnSession',
-    '/auth/isUserSocketConnect'
-], (req, res, next) => {
-    tokenHandler.validateUserAuthCookies(req) ? next() : res.sendStatus(401);
-}));
 
 // Import socket module and get the connected users object.
 let connectedUsers = require('./modules/sockets/socket')(io);
 
-//#region routes
-app.use("/api/login", require('./modules/routes/welcome/login'));
-app.use("/api/register", require('./modules/routes/welcome/register'));
-app.use("/api/forgotPassword", require('./modules/routes/welcome/forgotPassword'));
-app.use("/api/deleteUser", require('./modules/routes/deleteUser'));
-app.use("/api/navbar", require('./modules/routes/navbar'));
-app.use("/api/home", require('./modules/routes/home'));
-app.use("/api/chat", require('./modules/routes/chat'));
-app.use("/api/profilePicture", require('./modules/routes/profilePicture'));
-app.use("/api/userPage", require('./modules/routes/userPage/userPage'));
-app.use("/api/userEditWindow", require('./modules/routes/userPage/userEditWindow'));
-app.use("/api/userReportWindow", require('./modules/routes/userPage/userReportWindow'));
-app.use("/api/userPasswordWindow", require('./modules/routes/userPage/userPasswordWindow'));
-app.use("/api/userPrivacyWindow", require('./modules/routes/userPage/userPrivacyWindow'));
-app.use("/api/searchPage", require('./modules/routes/searchPage'));
-app.use("/api/management", permissionsMiddleware.root, require('./modules/routes/managementPanel/management'));
-app.use("/api/statistics", permissionsMiddleware.root, require('./modules/routes/managementPanel/statistics'));
-app.use("/api/usersReports", permissionsMiddleware.root, require('./modules/routes/managementPanel/usersReports'));
-app.use("/api/permissions", permissionsMiddleware.master, require('./modules/routes/managementPanel/permissions'));
-
-app.use("/api/auth", (req, res, next) => {
-    req.connectedUsers = connectedUsers;
-    next();
-}, require('./modules/routes/auth'));
-//#endregion
+// Import routes.
+require('./modules/routes/main')(app, connectedUsers);
 
 // Import server jobs and scripts.
 require('./modules/schedules')();
